fc4ff6acec5f9d1a1de2004d90e21463d5fef8f8
Gabriel Roper <9311953+Marsfan@users.noreply.github.com>
2024-01-15 20:36:37 -0700

    Switch to static binding

--- build.rs
+++ build.rs
@@ -11,14 +11,14 @@ fn main() {
         .file("src/bme68x_sensor_api/bme68x.c")
         .compile("bme68x_sensor_api");
 
-    // Generate BME68x bindings
-    let bme68x_bindings = bindgen::Builder::default()
-        .header("src/bme68x_bindings.h")
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Failed to generate bindings");
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bme68x_bindings
-        .write_to_file(out_path.join("bme68x_bindings.rs"))
-        .expect("Couldn't write bindings!");
+    // Generate BME68x_sensor_api bindings
+    // let bme68x_bindings = bindgen::Builder::default()
+    //     .header("src/bme68x_bindings.h")
+    //     .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+    //     .generate()
+    //     .expect("Failed to generate bindings");
+    // let out_path = PathBuf::from("./src");
+    // bme68x_bindings
+    //     .write_to_file(out_path.join("bme68x_bindings.rs"))
+    //     .expect("Couldn't write bindings!");
 }
